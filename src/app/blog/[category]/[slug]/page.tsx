4c2eb0effb228c095ebb714e56a7b0becf65b30c
Seung Hyun Tark <makepin2r@gmail.com>
2024-12-08 19:57:36 +0900

    update: 글 상세 페이지 page 컴포넌트 수정(임시 커밋)

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { PostDetail } from "@/config/types";
 import { getPostDetail, getPostPaths, parsePostAbstract } from "@/libs/post";
 import PostBody from "@/app/components/postDetail/PostBody";
 
-type PageProps = {
-  params: { category: string; slug: string };
-};
+interface PageParams {
+  category: string;
+  slug: string;
+}
 
 export const dynamicParams = false;
 
-export function generateStaticParams() {
+export function generateStaticParams(): { params: PageParams }[] {
   const postPaths: string[] = getPostPaths();
   const paramList = postPaths
     .map(path => parsePostAbstract(path))
-    .map(item => ({ category: item.categoryPath, slug: item.slug }));
+    .map(item => ({
+      params: { category: item.categoryPath, slug: item.slug },
+    }));
   return paramList;
 }
 
-const page = async ({ params: { category, slug } }: PageProps) => {
-  const post = await getPostDetail(category, slug);
+const page = async ({ params }: { params: PageParams }) => {
+  const { category, slug } = params;
+  const post: PostDetail = await getPostDetail(category, slug);
 
   return <PostBody post={post} />;
 };
