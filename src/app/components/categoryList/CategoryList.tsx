53bd1c111248bda658f7b09c7fe97d548c5ebb86
Seung Hyun Tark <makepin2r@gmail.com>
2024-12-09 13:13:53 +0900

    refactor: CategoryList 내 버튼 컴포넌트화

--- src/app/components/categoryList/CategoryList.tsx
+++ src/app/components/categoryList/CategoryList.tsx
@@ -1,8 +1,6 @@
 "use client";
-
-import { useRouter } from "next/navigation";
 import { CategoryDetail } from "@/config/types";
-import Badge from "../common/Badge";
+import CategoryButton from "./CategoryButton";
 
 interface CategoryListProps {
   categoryList: CategoryDetail[];
@@ -15,43 +13,25 @@ const CategoryList: React.FC<CategoryListProps> = ({
   currentCategory,
   allPostCount = 0,
 }) => {
-  const router = useRouter();
   return (
     <section className="w-full">
       <ul className="flex flex-wrap justify-start md:justify-center items-start gap-1">
-        <li>
-          <button
-            onClick={() => {
-              router.replace(`/blog`);
-            }}
-          >
-            <Badge type={currentCategory === undefined ? "primary" : "outline"}>
-              All
-              <span className="prose-caption-12-regular ml-1">
-                {allPostCount}
-              </span>
-            </Badge>
-          </button>
-        </li>
+        <CategoryButton
+          category={{
+            dirName: "",
+            publicName: "All",
+            count: allPostCount,
+          }}
+          badgeType={currentCategory === undefined ? "primary" : "outline"}
+        />
         {categoryList.map((category, i) => (
-          <li key={i}>
-            <button
-              onClick={() => {
-                router.replace(`/blog/${category.dirName}`);
-              }}
-            >
-              <Badge
-                type={
-                  currentCategory === category.dirName ? "primary" : "outline"
-                }
-              >
-                {category.dirName}
-                <span className="prose-caption-12-regular ml-1">
-                  {category.count}
-                </span>
-              </Badge>
-            </button>
-          </li>
+          <CategoryButton
+            key={i}
+            category={category}
+            badgeType={
+              currentCategory === category.dirName ? "primary" : "outline"
+            }
+          />
         ))}
       </ul>
     </section>
