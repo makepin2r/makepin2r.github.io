fc78b81991315c7493e297dfa5feaf1530f14765
Seung Hyun Tark <makepin2r@gmail.com>
2025-03-22 22:40:59 +0900

    update: PostBody 마크다운 문자열 받는 형태로 수정

--- src/components/postDetail/PostBody.tsx
+++ src/components/postDetail/PostBody.tsx
@@ -1,21 +1,21 @@
+"use-client";
 import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
-import { PostDetail } from "@/config/types";
 import { MdxComponents } from "../mdx/MdxComponents";
 
 interface PostBodyProps {
-  post: PostDetail;
+  post: string;
 }
 
 const PostBody: React.FC<PostBodyProps> = ({ post }) => {
   return (
     <MDXRemote
       components={MdxComponents}
-      source={post.content}
+      source={post}
       options={{
         mdxOptions: {
           remarkPlugins: [remarkGfm, remarkBreaks],
