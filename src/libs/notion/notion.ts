2cefc489bf41a0c529685ac4798ebd6467072de8
Seung Hyun Tark <makepin2r@gmail.com>
2025-03-23 20:43:54 +0900

    update: 페이지 목록 카테고리/노출여부 쿼리 필터링, 카테고리 리스트 불러오기 추가.

--- src/libs/notion/notion.ts
+++ src/libs/notion/notion.ts
@@ -3,19 +3,77 @@ import { MdStringObject } from "@/types";
 import {
   ListBlockChildrenResponse,
   QueryDatabaseResponse,
+  PageObjectResponse,
+  PartialPageObjectResponse,
+  PartialDatabaseObjectResponse,
+  DatabaseObjectResponse,
+  GetDatabaseResponse,
 } from "@notionhq/client/build/src/api-endpoints";
 
 /**
  * 데이터베이스 정보 가져오기
+ * @param category 카테고리명
+ * @returns QueryDatabaseResponse
  */
-export const getDatabase = async (): Promise<QueryDatabaseResponse> => {
-  return await notion.databases.query({
+const getDatabase = async (
+  category?: string,
+): Promise<QueryDatabaseResponse> => {
+  let query: any = {
+    database_id: process.env.NOTION_DB_ID as string,
+    filter: {
+      and: [
+        {
+          property: "published",
+          checkbox: {
+            equals: true,
+          },
+        },
+      ],
+    },
+  };
+
+  if (category) {
+    query.filter.and.push({
+      property: "category",
+      select: {
+        equals: category,
+      },
+    });
+  }
+
+  return await notion.databases.query(query);
+};
+
+// 데이터베이스 객체 정보 가져오기
+export const getDatabaseInfo = async (): Promise<GetDatabaseResponse> => {
+  return await notion.databases.retrieve({
     database_id: process.env.NOTION_DB_ID as string,
   });
 };
 
 /**
+ * 페이지 목록 가져오기
+ * @param category
+ * @returns Promise<(PageObjectResponse | PartialPageObjectResponse | PartialDatabaseObjectResponse | DatabaseObjectResponse)[]>
+ */
+export const getPostList = async (
+  category?: string,
+): Promise<
+  (
+    | PageObjectResponse
+    | PartialPageObjectResponse
+    | PartialDatabaseObjectResponse
+    | DatabaseObjectResponse
+  )[]
+> => {
+  const res = await getDatabase(category);
+  return res.results;
+};
+
+/**
  * 페이지 내용 가져오기
+ * @param pageId
+ * @returns ListBlockChildrenResponse
  */
 export const getPage = async (
   pageId: string,
@@ -25,6 +83,8 @@ export const getPage = async (
 
 /**
  * 단일 페이지 내용 markdown 문자열 형태로 가져오기
+ * @param pageId
+ * @returns MdStringObject
  */
 export const getPageMarkdown = async (
   pageId: string,
@@ -33,4 +93,27 @@ export const getPageMarkdown = async (
   const mdString = n2m.toMarkdownString(mdBlocks);
 
   return mdString;
+};
+
+/**
+ * 카테고리 정보 가져오기
+ */
+export const getPostCategories = async (): Promise<string[]> => {
+  const res = await getDatabaseInfo();
+  console.log("---- getPostCategories ----", res);
+  if (res.properties.category) {
+    // NOTE 현재 카테고리는 select 타입
+    // TODO type 처리 필요
+    const result = res.properties.category.select?.options.map(
+      (item: {
+        id: string;
+        name: string;
+        color: string;
+        description: string | null;
+      }) => item.name,
+    );
+    return result;
+  } else {
+    return [];
+  }
 };
