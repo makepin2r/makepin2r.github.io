ae086ece1451768b5d8ffd5c516b6fe66d151f06
Seung Hyun Tark <makepin2r@gmail.com>
2025-05-24 19:21:30 +0900

    refactor: 푸터 내용 자동 적용 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import Link from "next/link";
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { BLOG_USERID, GITHUB_URL, LINKEDIN_URL } from "@/config/const";
+
+const targetDate = new Date();
 
 const Footer = () => {
   return (
     <footer className="border-t-[1px] border-gray-100 w-full box-border">
       <div className="w-full max-w-[960px] mx-auto px-4 py-10">
         <div className="flex justify-center items-start gap-2">
-          <Link href="https://github.com/makepin2r" target="_blank">
+          <Link href={GITHUB_URL} target="_blank">
             <GitHubLogoIcon className="w-5 h-5 text-gray-700" />
           </Link>
-          <Link
-            href="https://www.linkedin.com/in/seunghyun-tark"
-            target="_blank"
-          >
+          <Link href={LINKEDIN_URL} target="_blank">
             <LinkedInLogoIcon className="w-5 h-5 text-gray-700" />
           </Link>
         </div>
         <p className="mt-2 prose-caption-12-regular text-gray-700 text-center">
-          © 2024. makepin2r All rights reserved.
+          © {targetDate.getFullYear()}. {BLOG_USERID} All rights reserved.
         </p>
       </div>
     </footer>
--- src/config/const.ts
+++ src/config/const.ts
@@ -1,5 +1,9 @@
 export const BLOG_TITLE = "일어나... 개발해야지";
 export const BLOG_DESCRIPTION = "프론트엔드 개발자 탁승현의 개발 블로그입니다.";
+export const BLOG_USERID = "makepin2r";
 
 export const BASE_DOMAIN = "https://makepin2r.github.io/";
 export const BASE_BLOG_URL = "/blog/";
+
+export const GITHUB_URL = "https://github.com/makepin2r";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/seunghyun-tark";
